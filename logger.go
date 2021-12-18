94a17adf5a9887da0755547f3fd21566b87e0847
Pastir Ivan <ivan@pastir.me>
2021-12-18 19:54:05 +0200

    Init module logger

---
+++ logger.go
@@ -0,0 +1,62 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+type ConfigLog struct {
+	//Path to log dir
+	LogDir string
+	//log file name
+	Filename string
+	//Max size(Mb) log file
+	MaxSize int
+	//The number of log files to store
+	MaxBackups int
+	//storage time(days) of log files
+	MaxAge int
+	//do need to archive files?
+	Compress   bool
+	Debug      bool
+	Warning    bool
+	Info       bool
+	Error      bool
+	Critical   bool
+	OutConsole bool
+	OutFile    bool
+
+	TimeKey    string
+	LevelKey   string
+	MessageKey string
+}
+
+func (c *ConfigLog) SetLog() *zap.Logger {
+	access := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   c.LogDir + "/" + c.Filename,
+		MaxSize:    c.MaxSize,
+		MaxBackups: c.MaxBackups,
+		MaxAge:     c.MaxAge, //days
+		Compress:   c.Compress})
+
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+			TimeKey:        c.TimeKey,
+			LevelKey:       c.LevelKey,
+			NameKey:        "",
+			CallerKey:      "",
+			MessageKey:     c.MessageKey,
+			StacktraceKey:  "",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.CapitalLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		}),
+		access,
+		zap.InfoLevel,
+	)
+
+	return zap.New(core)
+}
